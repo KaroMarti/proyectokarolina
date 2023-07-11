3d43d2e7576b43fc68ca66144fd90abb36e0d151
Karolina <kamg_20855@hotmail.com>
2023-07-10 22:01:13 -0500

    app-con-api-sinerror

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import Axios from 'axios'
+import { useEffect, useState } from 'react';
 
 const Home = () => {
     const {user, isAuthenticated, isLoading} = useAuth0();
+
+    const key = 'YI5N/XtqOtRlREc63OktNg==Pd5fI84y8YQtlYjx';
+    const headers = { 
+        'X-Api-Key': key
+
+    }
+    
+    const url='https://api.api-ninjas.com/v1/celebrity?name='+'Michael Jordan'
+
+    useEffect ( ()=>{
+        Axios.get (url)
+        .then(resp=>{console.log(resp.data)})
+        .catch(error=>{console.log(error)})
+    },[]
+
+    )
 
     if (isLoading) {
         return <div> Cargando ...</div>;
