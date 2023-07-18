bbc62aee59fb1e56b2ce6e3837942d7d01938871
Karolina <kamg_20855@hotmail.com>
2023-07-17 21:22:08 -0500

    con favorites

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import conf_auth from './conf_auth';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Auth0Provider {...conf_auth}>    
+    <Auth0Provider {...conf_auth}>   
+    <BrowserRouter>
     <App />
-    
+    </BrowserRouter> 
     </Auth0Provider>
     </Provider>
 
